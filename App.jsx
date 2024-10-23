a131467f04b4c57645e83c8054aa595ece5ae828
benjahuenchunir <bahuenchunir@gmail.com>
2024-10-22 21:59:57 -0300

    feat: main app

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import HabitCard from './components/HabitCard';
+
+export default function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <HabitCard name="Habito 1" onDelete={(habit) => console.log("Deleting " + habit)} />
+            <HabitCard name="Habito 2" onDelete={(habit) => console.log("Deleting " + habit)} />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    },
+});
