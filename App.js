b6f8e1aacfab0fbab565ba11e96be6a60584263d
benjahuenchunir <bahuenchunir@gmail.com>
2024-10-22 22:16:04 -0300

    refactor: change from inline to function

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import HabitCard from './components/HabitCard'; // Importa el componente HabitCard
 
 export default function App() {
+    function handleHabitDeletion(habit) {
+        console.log("Deleting " + habit);
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <HabitCard name="Habito 1" onDelete={(habit) => console.log("Deleting " + habit)} />
-            <HabitCard name="Habito 2" onDelete={(habit) => console.log("Deleting " + habit)} />
+            {/* Le pasamos el nombre del habito y la funcion que se ejecutara al apretar borrar */}
+            <HabitCard name="Habito 1" onDelete={handleHabitDeletion} /> 
+            <HabitCard name="Habito 2" onDelete={handleHabitDeletion} />
         </SafeAreaView>
     );
 }
